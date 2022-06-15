ab87ba6c97245a2ae7af31014eb9afabee080f0e
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2022-06-15 19:01:27 -0300

    Create hotel.java

---
+++ hotel.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class hotel {
+      
+      int D = 100, A = 10, N = 1, total;
+      
+    public static void main(String args[]) {
+      System in = new Scanner(System.in);
+
+      System.out.println("Insira o valor da diária: ");
+      D = in.nextInt();
+      
+      System.out.println("Insira o valor de aumento da diaria: ");
+      A = in.nextInt();
+      
+      System.out.println("Insira o dia da chegada no hotel: ");
+      N = in.nextInt();
+      
+      int promocao(int promo){
+        if(N >= 16){
+            return promo = D + (N - 1) * A;
+        }
+        return promo = D + 14 * A;
+      }
+      
+      total = (32 - N) * promocao;
+      
+      System.out.println("Esse é o valor total a ser pago ao hotel pela sua estadia: " +total);
+    }
+}
