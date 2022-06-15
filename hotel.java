68f4cba9721638efc3afb2416aa0778170d32f24
João Victor A <73762324+JoaoV-A01@users.noreply.github.com>
2022-06-15 19:02:56 -0300

    Update hotel.java

--- hotel.java
+++ hotel.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class hotel {
       
-      int D = 100, A = 10, N = 1, total;
+      int D, A, N, total;
       
     public static void main(String args[]) {
       System in = new Scanner(System.in);
